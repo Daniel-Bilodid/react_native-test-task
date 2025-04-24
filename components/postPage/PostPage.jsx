4b9e901040fec6407d5245afd3ff6136c4ac07e0
Daniel <danielbeloded@gmail.com>
2025-04-24 14:44:13 +0300

    finished with post page and now working with user authentication

--- components/postPage/PostPage.jsx
+++ components/postPage/PostPage.jsx
@@ -7,7 +7,7 @@ import {
   StyleSheet,
   ScrollView,
 } from "react-native";
-import NavComponent from "../navComponent/NavComponent";
+import ButtonComponent from "../../ui/ButtonComponent";
 
 export default function PostPage() {
   return (
@@ -23,10 +23,66 @@ export default function PostPage() {
 
           <View style={styles.titleWrapper}>
             <Text style={styles.postTitle}>Post name</Text>
+            <Image
+              style={styles.postImg}
+              source={require("../../assets/postImg.png")}
+            />
+          </View>
+        </View>
+
+        <View style={styles.about}>
+          <Text style={styles.aboutTitle}>About</Text>
+          <View style={styles.aboutTextWrapper}>
+            <Text style={styles.aboutText}>What is Lorem Ipsum?</Text>
+            <Text style={styles.aboutText}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley{" "}
+            </Text>
+          </View>
+        </View>
+
+        <View style={styles.comments}>
+          <Text style={styles.commentsTitle}>Comments</Text>
+          <View style={styles.commentsWrapper}>
+            <View style={styles.commentsCard}>
+              <Text style={styles.commentsCardName}>Egor</Text>
+              <Text style={styles.commentsCardEmail}>johndoe@test.com</Text>
+              <Text style={styles.commentsCardText}>
+                The investment objective is the highest possible increase in
+                value. The fund is composed of at least 2/3 of securities and
+                investment units of the gold sector. These include securities
+                linked to the development of the price of gold{" "}
+              </Text>
+            </View>
+
+            <View style={styles.commentsCard}>
+              <Text style={styles.commentsCardName}>Egor</Text>
+              <Text style={styles.commentsCardEmail}>johndoe@test.com</Text>
+              <Text style={styles.commentsCardText}>
+                The investment objective is the highest possible increase in
+                value. The fund is composed of at least 2/3 of securities and
+                investment units of the gold sector. These include securities
+                linked to the development of the price of gold{" "}
+              </Text>
+            </View>
+
+            <View style={styles.commentsCard}>
+              <Text style={styles.commentsCardName}>Egor</Text>
+              <Text style={styles.commentsCardEmail}>johndoe@test.com</Text>
+              <Text style={styles.commentsCardText}>
+                The investment objective is the highest possible increase in
+                value. The fund is composed of at least 2/3 of securities and
+                investment units of the gold sector. These include securities
+                linked to the development of the price of gold{" "}
+              </Text>
+            </View>
           </View>
         </View>
       </ScrollView>
-      <NavComponent />
+      <View style={styles.postButtonWrapper}>
+        <ButtonComponent text="Back" />
+      </View>
     </>
   );
 }
@@ -37,11 +93,11 @@ const styles = StyleSheet.create({
   },
   postHeader: {
     position: "relative",
-    height: 296,
+    height: 480,
     borderRadius: 28,
     overflow: "hidden",
     backgroundColor: "rgb(255, 255, 255)",
-    justifyContent: "center",
+    justifyContent: "flex-end",
   },
   backButton: {
     position: "absolute",
@@ -55,8 +111,95 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   postTitle: {
-    fontSize: 18,
+    fontSize: 28,
     fontWeight: "600",
     color: "rgb(6, 7, 10)",
+    fontWeight: 700,
+    lineHeight: 32,
+  },
+  titleWrapper: {
+    flexDirection: "column",
+
+    alignItems: "center",
+  },
+  postImg: {
+    marginBottom: 27,
+    marginTop: 42,
+  },
+  about: {
+    marginTop: 24,
+    marginLeft: 16,
+    marginRight: 16,
+  },
+  aboutTitle: {
+    color: "rgb(96, 103, 115)",
+    fontSize: 15,
+    fontWeight: 400,
+    lineHeight: 16,
+  },
+  aboutTextWrapper: {
+    marginTop: 8,
+    width: "100%",
+    height: 240,
+    backgroundColor: "rgb(255, 255, 255)",
+    borderRadius: 16,
+    padding: 24,
+  },
+  aboutText: {
+    color: "rgb(6, 7, 10)",
+    fontSize: 15,
+    fontWeight: 400,
+    lineHeight: 32,
+  },
+
+  comments: {
+    marginTop: 48,
+    marginLeft: 16,
+    marginRight: 16,
+    paddingBottom: 20,
+  },
+  commentsWrapper: {
+    gap: 12,
+  },
+  commentsTitle: {
+    color: "rgb(96, 103, 115)",
+    fontSize: 15,
+    fontWeight: 400,
+    lineHeight: 16,
+    marginBottom: 8,
+  },
+  commentsCardWrapper: {
+    gap: 12,
+  },
+  commentsCard: {
+    width: "100%",
+    padding: 12,
+    backgroundColor: "rgb(255, 255, 255)",
+    borderRadius: 16,
+  },
+  commentsCardName: {
+    color: "rgb(0, 0, 0)",
+    fontSize: 18,
+    fontWeight: 500,
+    lineHeight: 24,
+  },
+  commentsCardEmail: {
+    marginTop: 2,
+    color: "rgb(6, 7, 10)",
+    fontSize: 15,
+    fontWeight: 500,
+    lineHeight: 24,
+  },
+  commentsCardText: {
+    marginTop: 12,
+    color: "rgb(6, 7, 10)",
+    fontSize: 14,
+    fontWeight: 400,
+    lineHeight: 19,
+  },
+  postButtonWrapper: {
+    width: "100%",
+    height: 98,
+    backgroundColor: "rgb(255, 255, 255)",
   },
 });
